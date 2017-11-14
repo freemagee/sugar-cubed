2bb898166000641dc6c334b2e64a40702eff3754
Neil Magee <info@neilmagee.com>
2017-11-14 17:07:27 +0000

    First test to see a custom input emit it's value

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -3,7 +3,7 @@ Vue.component("search", {
   template: `<div class="form form--horizontal">
       <div class="form__item">
         <label for="foodSearch" class="form__label">Search for food</label>
-        <input type="text" class="form__input form__input--text" id="foodSearch" v-bind:value="value" v-on:input="logValue($event.target.value)" />
+        <input type="text" class="form__input form__input--text" id="foodSearch" v-bind:value="value" v-on:input="updateValue($event.target.value)" ref="input" />
       </div>
       <div class="form__item">
         <label for="brandedFoods" class="form__label">Search branded foods?</label>
@@ -11,8 +11,15 @@ Vue.component("search", {
       </div>
     </div>`,
   methods: {
-    logValue: (value) => {
-      console.log(value);
+    updateValue: function(value) {
+      let formattedValue = value.trim();
+      // If the value was not already normalized,
+      // manually override it to conform
+      if (formattedValue !== value) {
+        this.$refs.input.value = formattedValue;
+      }
+      // Emit the number value through the input event
+      this.$emit("input", String(formattedValue));
     }
   }
 });
