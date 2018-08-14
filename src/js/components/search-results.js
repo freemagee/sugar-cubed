27abbefb4779860ae0184ba3dc40754446a82e73
Neil Magee <info@neilmagee.com>
2018-08-14 15:26:57 +0100

    Style search results list

--- src/js/components/search-results.js
+++ src/js/components/search-results.js
@@ -15,17 +15,22 @@ Vue.component("search-results", {
     }
   },
   template: `
-  <section v-if="foodList.length > 0">
-    <div id="searchResults">
-      <nav id="foodList">
-        <a
-          v-for="food in foodList"
-          :value="food.name"
-          :key="food.ndbno"
-          @click="selectFood(food)"
-          :class="{ 'is-active' : food.ndbno === selectedId }"
-        >{{ food.name }}</a>
-      </nav>
+  <section v-if="foodList.length > 0" class="overflow-auto vh-50 ph2 ph4-ns ph5-l pb4">
+    <div id="searchResults" class="flex flex-column">
+      <button
+        v-for="(food, foodIndex) in foodList"
+        :value="food.name"
+        :key="food.ndbno"
+        :class="{
+          'dark-gray bg-white hover-bg-light-gray' : food.ndbno !== selectedId,
+          'white bg-caribbean-green' : food.ndbno === selectedId,
+          'bb' : foodIndex === foodList.length - 1,
+          'bb-0' : foodIndex !== foodList.length - 1
+        }"
+        @click="selectFood(food)"
+        class="button-reset flex-auto tl segoe-ui pv2 ph3 bl bt br b--black-10 pointer bg-animate"
+        type="button"
+      >{{ food.name }}</button>
     </div>
   </section>`
 });
