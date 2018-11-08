2a7a05419209090776f2e336832ce0774f99a721
Neil Magee <info@neilmagee.com>
2018-11-08 13:02:42 +0000

    Add flexbox tachyons classes

--- src/js/components/information.js
+++ src/js/components/information.js
@@ -13,30 +13,26 @@ Vue.component("information", {
   <section
     v-if="name !== ''"
     id="foodInformation"
+    class="overflow-auto overflow-hidden-ns flex-ns"
   >
-    <div class="mb2 mb3-ns">
-      <h3 class="ma0 segoe-ui normal f3 lh-title light-gray">{{ name }}</h3>
-    </div>
-
-    <div
-      v-if="nutrition.totalSugars === 0 || nutrition.totalSugars === '0.00'"
-      class="mb3"
-    >
-      <p class="ma0 segoe-ui f4 light-gray">Sugar free!</p>
-    </div>
-
-    <div
-      v-else
-      class="mb3"
-    >
-      <p class="ma0 segoe-ui f4 light-gray">
+    <div class="mb2 mb0-ns fg0 fs1 fb-50">
+      <h3 class="mt0 mb3 segoe-ui normal f3 lh-title light-gray">{{ name }}</h3>
+      <p
+        v-if="nutrition.totalSugars === 0 || nutrition.totalSugars === '0.00'"
+        class="ma0 segoe-ui f4 light-gray"
+      >
+        Sugar free!
+      </p>
+      <p
+        v-else
+        class="ma0 segoe-ui f4 light-gray"
+      >
         <strong>Measure: </strong>per 100 grams<br />
         <strong>Grams of sugar: </strong>{{ nutrition.totalSugars }}<br />
         <strong>Sugar cubes: </strong>{{ nutrition.wholeCubes + (nutrition.remainderCube * 1) }}
       </p>
     </div>
-
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap fg0 fs1 fb-50">
       <div
         v-for="(cube, cubeIndex) in nutrition.wholeCubes"
         :key="cubeIndex"
@@ -57,6 +53,5 @@ Vue.component("information", {
         class="mb2 bg-near-white ba b--black-30 b--dotted bw2"
       ></div>
     </div>
-
   </section>`
 });
