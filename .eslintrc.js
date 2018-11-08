018f0961a15712f4678303bb4f8b00b2dd55bdcf
Neil Magee <info@neilmagee.com>
2018-11-08 12:13:17 +0000

    Add lint tooling

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    "plugin:vue/essential",
+    "eslint-config-prettier"
+  ],
+  plugins: ["eslint-plugin-prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+};
