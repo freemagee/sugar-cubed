297864e425f0fd20b20e27599b46203e6406991d
Neil Magee <info@neilmagee.com>
2018-11-14 11:44:19 +0000

    Make better use of prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,36 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 6
+    parser: 'babel-eslint'
   },
   env: {
-    es6: true,
     browser: true
-  },
-  extends: ["eslint-config-airbnb-base", "eslint-config-prettier", "plugin:vue/essential"],
-  plugins: ["eslint-plugin-import", "eslint-plugin-prettier"],
-  rules: {
-    "prettier/prettier": ["error", {}],
-    "no-new": 0,
-    "max-len": 0,
-    "linebreak-style": 0,
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    indent: ["error", 2, { SwitchCase: 1 }],
-    "import/no-unresolved": 0,
-    strict: 0,
-    "no-unused-vars": ["error", { varsIgnorePattern: "css" }],
-    "no-param-reassign": ["error", { "props": false }]
   },
   globals: {
     app: true,
     Vue: true
+  },
+  extends: [
+    "prettier",
+    "plugin:vue/recommended", // /base, /essential, /strongly-recommended, /recommended
+    "plugin:prettier/recommended", // turns off all ESLINT rules that are unnecessary due to Prettier or might conflict with Prettier.
+    "eslint:recommended"
+  ],
+  plugins: ["vue", "prettier"],
+  rules: {
+    //"prettier/prettier": "error",
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "never",
+          normal: "always",
+          component: "any"
+        },
+        svg: "always",
+        math: "always"
+      }
+    ],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off"
   }
 };
